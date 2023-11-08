a1cf76712effbdfc11622c0c76d082e1780bfc0b
elanore <elanorejoseph@gmail.com>
2023-11-08 13:04:20 +0530

    understanding components

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import React from "react";
 import ReactDOM  from "react-dom/client"; 
 
-  
-  /*
+//jsx - can be nested ,wrap it inside a single div
 
-   nesting in react 
-   div>div>h1
+//react element😎
+const heading = (
+<h1 id="heading" className="head" tabIndex="1">
+    Hello React 🙂  
+</h1>
+);
+console.log(heading,"heading");
 
-   const parent = React.createElement("div",{id:"parent},
-    React.createElement("div",{id:"child"},
-    React.createElement("h1",{},"i am h1 tag")
-    )
-   )
+//React component -everything is a component in react 
+//React functional component is just a normal js function- transpile to js function 
+//both syntax below are same
+const Headingcomp1 = () => <h1>Hello functions</h1>;
 
-   */
-   
-  
-   const heading  = React.createElement("h1",{id:"heading"},"hello world React");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+
+//React functional component😎
+const Headingcomp = () => {
+ return <h1>Hello functional components 😘</h1>;
+};
+
+//component composition -composing component inside 
+const Title =() =>(
+    
+    <div>
+        <Headingcomp/>
+        <Headingcomp1/>
+        <h2>hello nesting</h2>
+    </div>
+    
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//renders react element 
+
+//how to render components
+root.render(<Title/>);
