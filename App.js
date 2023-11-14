28cbea0f32b0d329a3f801a880a1515e6c2e7140
elanore <elanorejoseph@gmail.com>
2023-11-14 14:57:42 +0530

    adding res card to app layout

--- App.js
+++ App.js
@@ -9,6 +9,11 @@ import ReactDOM  from "react-dom/client";
     -search
     -restaurant container
       -restaurant cards
+            - img 
+            - name
+            - star rating
+            - cuisine
+            - ETA
 
 * footer
     -copyright
@@ -28,6 +33,7 @@ const Header = () =>{
             </div>
             <div className="nav-items">
                 <ul>
+                    <li>Home</li>
                     <li>About us</li>
                     <li>Contact us</li>
                     <li>Cart</li>
@@ -39,10 +45,37 @@ const Header = () =>{
 
 }
 
+const RestaurantCard = () =>{
+    return (
+        <div className="res-card">
+            <img 
+            className="res-logo"
+            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"  alt="megha biriyani"/>
+            <h3>Meghna Foods</h3>
+
+        </div>
+
+
+    )
+
+}
+
+const Body = () =>{
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
 const AppLayout = () =>{
     return (
         <div className="app">
             <Header/>
+            <Body/>
 
         </div>
     );
