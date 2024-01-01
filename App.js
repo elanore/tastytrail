dd14524899f253dc5cfc089ff85c78045dc13a88
elanore <elanorejoseph@gmail.com>
2024-01-01 22:43:53 +0530

    dynamic data

--- App.js
+++ App.js
@@ -45,19 +45,19 @@ const Header = () =>{
 
 };
 
-const RestaurantCard = () =>{
+const RestaurantCard = (props) =>{
     return (
-        <div className="res-card" style={{backgroundColor:"f0f0f0"}}>
+        <div className="res-card" /*style={{backgroundColor: "#f0f0f0"}}*/>
             <img 
             className="res-logo"
-            alt="megha biriyani"
+            alt="meghna biriyani"
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf" 
             
             />
-            <h3>Meghna Foods</h3>
-            <h4>Biriyani, North Indian, Chinese</h4>
-            <h4>4.4 stars</h4>
-            <h4>38 minutes</h4>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.rating}</h4>
+            <h4>{props.ETA}</h4>
         </div>
     );
 
@@ -68,30 +68,9 @@ const Body = () =>{
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+                <RestaurantCard resName="Meghna Foods" cuisine="Biriyani, Andra Foods" rating="4.4" ETA="20min"/>
+                <RestaurantCard resName="KFC" cuisine="American, Fast Food" rating="3.6" ETA="30mins"/>
+               
             </div>
         </div>
     );
