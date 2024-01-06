290514a2448d5b280992effae7953625e3e6ff49
elanore <elanorejoseph@gmail.com>
2024-01-06 21:45:58 +0530

    adding onclick button

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tastytrail
-React learning 
+
+React learning
+
+food ordering app layout
+
+- header
+  -logo
+  -nav item component
+- body
+  -search
+  -restaurant container
+  -restaurant cards - img - name - star rating - cuisine - ETA
+
+- footer
+  -copyright
+  -address
+  -contact
+  -links
+
+\*/
--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,10 @@ import resList from "../utils/mockData";
 const Body = () =>{
     return(
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={()=>{console.log("clicked")}}>
+                    Top Rated Restaurants</button>
+             </div>
             <div className="res-container">
                 {
                     resList.map((restaurant)=>
