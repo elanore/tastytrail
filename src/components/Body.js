63ac091e0a08d2a65a97ae342f9c38f450455d29
elanore <elanorejoseph@gmail.com>
2024-01-08 22:14:42 +0530

    adding filter condition

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,15 +3,50 @@ import resList from "../utils/mockData";
 
 
 const Body = () =>{
+    
+    let listofRestaurant = [
+        {
+                    "info": {
+                      "id": "65797",
+                      "name": "Leon's - Burgers & Wings (Leon Grill)",
+                      "cloudinaryImageId": "b2edbc28b7b8219d6e0a9c049ce06658",
+                      "costForTwo": "₹300 for two",
+                      "cuisines": ["American","Snacks","Turkish","Portuguese","Continental"],
+                      "avgRating": 4.4,
+                    },
+        },
+        {
+                    "info": {
+                      "id": "65798",
+                      "name": "Hello Burger",
+                      "cloudinaryImageId": "b2edbc28b7b8219d6e0a9c049ce06658",
+                      "costForTwo": "₹350 for two",
+                      "cuisines": ["American","Snacks","Turkish","Portuguese","Continental"],
+                      "avgRating": 3.2,
+                    }, 
+        },
+];
+
     return(
         <div className="body">
             <div className="filter">
-                <button className="filter-btn" onClick={()=>{console.log("clicked")}}>
-                    Top Rated Restaurants</button>
+                <button 
+                className="filter-btn" 
+                onClick={()=>{
+                    //filter logic
+                    //console.log("clicked");
+                    listofRestaurant = listofRestaurant.filter(
+                        (res)=>res.info.avgRating > 4
+                        );
+                        console.log(listofRestaurant);
+                    }}
+                >
+                    Top Rated Restaurants
+                </button>
              </div>
             <div className="res-container">
                 {
-                    resList.map((restaurant)=>
+                    listofRestaurant.map((restaurant)=>
                     (
                     <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                     ))
