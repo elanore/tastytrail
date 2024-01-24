d8a69719d6ef3b2b5655d17254b24e71879d790d
elanore <elanorejoseph@gmail.com>
2024-01-24 20:56:07 +0530

    understanding class components

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,8 @@ import { useState } from "react";
 
 const User = (props) =>{
 
-    const [count] = useState(0)
+    const [count,setCount] = useState(0);
+    const [count1] = useState(1);
 
     return (<div className="user-card">
         
@@ -12,6 +13,7 @@ const User = (props) =>{
         <p>Name: {props.name}</p>
         <p>Contact : www.linkedIn/shiny-joseph</p>
         <h2>Count = {count}</h2>
+        <h2>Count1 = {count1}</h2>
 
     </div>
     );
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,6 +7,7 @@ class UserClass extends React.Component{
 
         this.state = {
             count : 1,
+            
         };
 
        
@@ -14,13 +15,28 @@ class UserClass extends React.Component{
     //react.component is a class given by react 
     render(){
 
+        const {name,location} = this.props;
+        const {count,count2} = this.state;
+
 
         //render method returns a jsx which is displayed in ui
         return (<div className="user-card">
         <h1>About user-class</h1>
-        <h2>count = {this.state.count}</h2>
-        <p>Name : {this.props.name}</p>
-        <p>Location: {this.props.location}</p>
+        <h2>count = {count}</h2>
+        <button onClick={()=>{
+            this.setState({
+                count : this.state.count +1
+            });
+        }}>Count + </button>
+        <button onClick= {()=>{
+            this.setState(
+                {
+                    count :this.state.count -1
+                }
+            );
+        }}>Count -</button>
+        <p>Name : {name}</p>
+        <p>Location: {location}</p>
         <p>Contact : www.linkedIn/shiny-joseph</p>
 
     </div>
