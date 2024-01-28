b920d918802861246553035b17490896eb91ded6
elanore <elanorejoseph@gmail.com>
2024-01-28 17:52:45 +0530

    custom hook for data fetch

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,43 +1,27 @@
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 
 const RestaurantMenu = ()=>{
-    const [resInfo,setresInfo] =useState(null);
-
     const {resId} = useParams();
-    console.log(resId,"params");
+    //console.log(resId,"params");
+    const resInfo = useRestaurantMenu(resId);
 
-    
-    useEffect(() =>{
-        fetchMenu();
-
-    },[]);
-
-
-    
-
-const fetchMenu = async () =>{
-    const data = await fetch(MENU_API +resId);
-    const json = await data.json();
-    //console.log(json);
-    //console.log(json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards,"id item")
-    //console.log(json.data.cards[0].card.card.info);
-    setresInfo(json.data);
-};
+    //shimmer
 if(resInfo ===null) return <Shimmer/>;
 
 
-//destructuring data for menu items
-const { itemCards } = resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
-//console.log(itemCards,"itemCards");
-    
+  
 
 //destructure data for name ,cuisines avgRating
 const {name, cuisines, costForTwoMessage, avgRating}  = resInfo?.cards[0]?.card?.card?.info;
-    
+ 
+//destructuring data for menu items
+const { itemCards } = resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
+//console.log(itemCards,"itemCards");
+  
     return (  
         <div className="menu">
             <h1>{name}</h1>
@@ -47,7 +31,10 @@ const {name, cuisines, costForTwoMessage, avgRating}  = resInfo?.cards[0]?.card?
              
             <ul>
                 {itemCards.map((item) => (
-                    <li key={item.card.info.id}>{item.card.info.name} - {item.card.info.price/100} </li>
+                    <li key={item.card.info.id}>
+                        {item.card.info.name} - {"Rs."}
+                        {item.card.info.price/100 || item.card.info.defaultPrice} 
+                    </li>
                 ))}
             </ul>
         </div>
